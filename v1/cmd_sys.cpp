e33ccf6bd514d39a0794827aed9c62b088ede6d1
SoliGhost <3938753633@qq.com>
2024-10-17 15:20:52 +0800

    commit v1

--- v1/cmd_sys.cpp
+++ v1/cmd_sys.cpp
@@ -13,17 +13,17 @@ void cmd_help_fun(vector<string> args)
 {
     if (args.size() > 1)
     {
-        cout << "[ERROR] help只需要0或1个参数，输入了" << args.size() << "个参数。需要帮助请输入help help\n";
+        cout << "[ERROR] help只需要0或1个参数，输入了" << args.size() << "个参数。需要帮助请输入\"help help\"\n";
         return;
     }
     else if (args.size() == 0)
     {
-        cout << "Program General Help Document" << endl;
+        cout << "<Program General Help Document>" << endl;
         return;
     }
     if (cmd_id_map.find(args[0]) == cmd_id_map.end())
     {
-        cout << "不存在命令: \"" << args[0] << "\"。需要帮助请输入help help\n";
+        cout << "不存在命令: \"" << args[0] << "\"。需要帮助请输入\"help help\"\n";
         return;
     }
     int cmd_id = cmd_id_map.at(args[0]);
@@ -45,7 +45,7 @@ void cmd_help_fun(vector<string> args)
         cout << R"TAG(Document of fun2
 )TAG";
     default:
-        cout << "[ERROR] \"" << args[0] << "\" Command id not found. Please check your program.\n";
+        cout << "命令\"" << args[0] << "\"没有帮助文档\n";
     }
 }
 
@@ -69,7 +69,7 @@ void cmd_fun2_fun(vector<string> args)
 
 void cmd_fun(string cmd, vector<string> args) {
     if(cmd_id_map.find(cmd)==cmd_id_map.end()) {
-        cout << "Unknown command: \"" << cmd << "\"" << endl;
+        cout << "不存在该命令: \"" << cmd << "\"" << endl;
         return;
     }
     int cmd_id = cmd_id_map.at(cmd);
@@ -84,7 +84,7 @@ void cmd_fun(string cmd, vector<string> args) {
         cmd_fun2_fun(args);
         break;
     default:
-        cout << "[ERROR] \""<< cmd <<"\" Command id not found. Please check your program." << endl;
+        cout << "[ERROR] ID为" << cmd_id << "的命令\""<< cmd <<"\"没有对应的执行函数，请检查你的源代码" << endl;
         break;
     }
 }
--- v1/cmd_sys.h
+++ v1/cmd_sys.h
@@ -7,11 +7,11 @@
 
 const std::map<std::string, int> cmd_id_map = {
     {"help", 0},
-    {"?", 0},
+    {"?",    0},
     {"fun1", 1},
-    {"1", 1},
+    {"1",    1},
     {"fun2", 2},
-    {"2", 2}
+    {"2",    2}
 };
 
 void cmd_fun(std::string cmd, std::vector<std::string> args);
